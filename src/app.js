5833e0cda13cfc650c34686ef69912e9a6ba294f
William Bergeron (2052824) <2052824@cstj.qc.ca>
2022-09-01 15:00:58 -0400

    Operation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import dayjs from "dayjs";
 
 const app = express();
 
@@ -11,6 +12,51 @@ app.get('/premiere', (req, res) => {
     res.send('Première route avec express');
 });
 
+app.get('/date', (req, res) => {
 
+    res.status(200);
+    res.set('Content-Type', 'text/plain');
+    const today = dayjs(); 
+    res.send(today.format());
+});
+
+app.get('/maths/:operation', (req, res)=> {
+    const a = parseInt(req.query.a, 10);
+    const b = parseInt(req.query.b, 10);
+    
+    const operation = req.params.operation;
+    let result = 0;
+    const somme = (a+b).toString();
+
+    switch (operation) {
+        case 'somme':
+            result = (a+b).toString();
+            break;
+
+        case 'soustraction':
+            result = (a-b).toString();
+            break;
+
+        case 'division':
+            result = (a/b).toString();
+            break;
+        
+        case 'multiplication':
+            result = (a*b).toString();
+            break;
+
+        case 'modulo':
+            result = (b%a).toString();
+            break;
+    
+        default:
+            result = 'Opération non définie';
+            break;
+    }
+
+    res.status(200);
+    res.set('Content-Type', 'text/plain');
+    res.send(result);
+});
 
 export default app;
