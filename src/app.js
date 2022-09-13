7f10e66b12768f83fd32a318c3258555f7bc2dc5
William Bergeron (2052824) <2052824@cstj.qc.ca>
2022-09-13 09:43:24 -0400

    DatabaseStart2

--- src/app.js
+++ src/app.js
@@ -10,62 +10,6 @@ database();
 const app = express();
 app.use(express.json()); // Permettre à notre serveur de comprendre le json reçu
 
-//TODO: Ajouter du code ici
-
-app.get('/premiere', (req, res) => {
-
-    res.status(200);
-    res.set('Content-Type', 'text/plain');
-    res.send('Première route avec express');
-});
-
-app.get('/date', (req, res) => {
-
-    res.status(200);
-    res.set('Content-Type', 'text/plain');
-    const today = dayjs(); 
-    res.send(today.format());
-});
-
-app.get('/maths/:operation', (req, res)=> {
-    const a = parseInt(req.query.a, 10);
-    const b = parseInt(req.query.b, 10);
-    
-    const operation = req.params.operation;
-    let result = 0;
-    const somme = (a+b).toString();
-
-    switch (operation) {
-        case 'somme':
-            result = (a+b).toString();
-            break;
-
-        case 'soustraction':
-            result = (a-b).toString();
-            break;
-
-        case 'division':
-            result = (a/b).toString();
-            break;
-        
-        case 'multiplication':
-            result = (a*b).toString();
-            break;
-
-        case 'modulo':
-            result = (b%a).toString();
-            break;
-    
-        default:
-            result = 'Opération non définie';
-            break;
-    }
-
-    res.status(200);
-    res.set('Content-Type', 'text/plain');
-    res.send(result);
-});
-
 app.use('/planets', planetsRoutes);
 
 app.use(errors);
