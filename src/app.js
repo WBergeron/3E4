40ff91ae026a7e3200e06a77ca0195ba93484f52
William Bergeron (2052824) <2052824@cstj.qc.ca>
2022-09-13 09:43:02 -0400

    DatabaseStart

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import dayjs from "dayjs";
 import planetsRoutes from './routes/planets-routes.js';
 
+import database from './libs/database.js';
+
 import errors from "./middlewares/errors.js";
 
+database();
 const app = express();
 app.use(express.json()); // Permettre à notre serveur de comprendre le json reçu
 
